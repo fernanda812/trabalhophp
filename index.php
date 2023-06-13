62d6d8da2fb493b37d0ac0019617d2187ae49607
fernanda812 <fernandasoaresnunes128@gmail.com>
2023-06-13 16:58:26 -0300

    Criando tabela

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unidades federativas do Brasil</title>
+</head>
+
+<body>
+    <h1>Tabela das Unidades Federativas do Brasil</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Bandeira</th>
+                <th>Unidade Federativa</th>
+                <th>Abreviação</th>
+                <th>Sede de Governo</th>
+                <th>Área(km²)</th>
+                <th>População(2014)</th>
+                <th>Densidade(2005)</th>
+                <th>PIB(2015)</th>
+                <th>(&#37;Total)(2015)</th>
+                <th>PIB per capita(R&#36;)(2015)</th>
+                <th>IDH(2010)</th>
+                <th>Alfabetização(2016)</th>
+                <th>Mortalidade Infantil(2016)</th>
+                <th>Expectativa de Vida(2016)</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+
+
+    </table>
+
+</body>
+
+</html>
